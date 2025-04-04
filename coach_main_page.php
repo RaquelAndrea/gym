0546b4ae418685e8e3424f2e72c9c7eadfbc9c89
tjkyro <travisjam18@gmail.com>
2025-04-04 11:35:32 -0400

    Update coach_main_page.php
    updated fixed html tags and body

--- coach_main_page.php
+++ coach_main_page.php
@@ -1,24 +1,24 @@
-<Html>
+<html>
 
 <head>
-<style>
+ <style>
     html{
     color-scheme: dark;
     }
 
-</style>
+ </style>
 </head>
 
-
+ <body>
 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT243RYpMJZ7i_O8XB1NJMTlB2ZybjjLCrTVg&s" alt="WAU logo" width="100"/>
 <h1>Adminsitration Page</h1>
 <hr/>
-<table >
+  <table >
     <tr valign = "center">
         <td >
        
 
-<ul>
+            <ul>
                 <li><a href="sign_up.php" title="Signup">Sign Up</a>
                 <li><a href="faq_page.php" title="Faq">FAQ Page</a>
                 <li><a href="suggestion.php" title="Suggestions">Suggestions/Complaints</a>
@@ -26,7 +26,8 @@
                 
             </ul>
         </td>
-    </tr>
-    </table>
-     </Html>
+     </tr>
+</table>
+ </body>
+</html>
 
