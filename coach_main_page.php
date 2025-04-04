ac8212c5b0a3c4ccd2e73cfba0c5cd77be707eb0
tjkyro <travisjam18@gmail.com>
2025-04-04 11:23:34 -0400

    Update coach_main_page.php
    File created in class SWE class

--- coach_main_page.php
+++ coach_main_page.php
@@ -1,13 +1,32 @@
-
 <Html>
-    <head>
-    </head>
 
-    <h1 style="text-align: center;">Administration Page</h1>
-    
+<head>
+<style>
+    html{
+    color-scheme: dark;
+    }
 
-    
-   
-</html>
+</style>
+</head>
 
+
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT243RYpMJZ7i_O8XB1NJMTlB2ZybjjLCrTVg&s" alt="WAU logo" width="100"/>
+<h1>Adminsitration Page</h1>
+<hr/>
+<table >
+    <tr valign = "center">
+        <td >
+       
+
+<ul>
+                <li><a href="sign_up.php" title="Signup">Sign Up</a>
+                <li><a href="faq_page.php" title="Faq">FAQ Page</a>
+                <li><a href="suggestion.php" title="Suggestions">Suggestions/Complaints</a>
+                <li><a href="logout.php" title="Logout">logout</a>
+                
+            </ul>
+        </td>
+    </tr>
+    </table>
+     </Html>
 
