d4d468468587712d3153146e1d312d5ca3585244
RaquelAndrea <praquel2003@gmail.com>
2025-04-04 12:00:12 -0400

    Update coach_main_page.php (#4)

--- coach_main_page.php
+++ coach_main_page.php
@@ -1,34 +1,30 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html>
-
 <head>
- <style>
-    html{
-    color-scheme: dark;
+  <style>
+    html {
+      color-scheme: dark;
     }
-
- </style>
+  </style>
 </head>
+<body>
 
- <body>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT243RYpMJZ7i_O8XB1NJMTlB2ZybjjLCrTVg&s" alt="WAU logo" width="100"/>
-<h1>Adminsitration Page</h1>
-<hr/>
-  <table >
-    <tr valign = "center">
-        <td >
-       
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT243RYpMJZ7i_O8XB1NJMTlB2ZybjjLCrTVg&s" alt="WAU logo" width="100"/>
+  <h1>Administration Page</h1>
+  <hr/>
 
-            <ul>
-                <li><a href="sign_up.php" title="Signup">Sign Up</a>
-                <li><a href="faq_page.php" title="Faq">FAQ Page</a>
-                <li><a href="suggestion.php" title="Suggestions">Suggestions/Complaints</a>
-                <li><a href="logout.php" title="Logout">logout</a>
-                
-            </ul>
-        </td>
-     </tr>
-</table>
- </body>
+  <table>
+    <tr valign="center">
+      <td>
+        <ul>
+          <li><a href="sign_up.php" title="Signup">Sign Up</a></li>
+          <li><a href="faq_page.php" title="Faq">FAQ Page</a></li>
+          <li><a href="suggestion.php" title="Suggestions">Suggestions/Complaints</a></li>
+          <li><a href="logout.php" title="Logout">Logout</a></li>
+        </ul>
+      </td>
+    </tr>
+  </table>
+
+</body>
 </html>
-   
