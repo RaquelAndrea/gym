72c1ac5082d94a191a5559f11a81599b39f4e868
tjkyro <travisjam18@gmail.com>
2025-04-04 17:42:42 -0400

    Add files via upload

---
+++ invalid_id.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Invalid ID
+    </title>
+
+    <style>
+        html{
+            color-scheme:dark;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Wau Athletics</h1>
+    <p>Invalid ID#</p>
+
+    <a href="signin.php">
+        <button>GO BACK</button>
+    </a>
+</body>
+</html>
---
+++ successfullylogin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title> 
+        LOGIN SUCCESSFUL.
+      </title>
+      <style>
+        html{
+            color-scheme: dark;
+        }
+      </style>
+    </head>
+
+
+    <body>
+         <h1>Wau Athletics</h1> 
+            <p>You have successfully signed in!</p>
+    
+            <a href="mainpagewau.php">
+                <button>GO TO MAIN</button> 
+            </a>
+       
+    
+    </body>
+
+
+
+
+
+
+
+
+
+</html> 
