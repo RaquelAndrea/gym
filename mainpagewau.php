0072d662322a86d34270a87177e1ec42836eca43
RaquelAndrea <praquel2003@gmail.com>
2025-04-06 15:02:40 -0400

    Update mainpagewau.php

--- mainpagewau.php
+++ mainpagewau.php
@@ -1,10 +1,10 @@
 <html>
     <body>
-        <p><a href="student_sign_in.php" title="Student Sign In">Student Sign In</p>
-        <p><a href="feedback.php" title="Feedback">Feedback</p>
-        <p><a href="crowd_meter.php" title="Crowd Meter">Crowd Meter</p>
-        <p><a href="schedule.php" title="Schedule">Schedule</p>
+        <p><a href="student_sign_in.php" title="Student Sign In">Student Sign In</a></p>
+        <p><a href="feedback.php" title="Feedback">Feedback</a></p>
+        <p><a href="crowd_meter.php" title="Crowd Meter">Crowd Meter</a></p>
+        <p><a href="schedule.php" title="Schedule">Schedule</a></p>
         <p><a href="payroll.php" title="FAQ">FAQ</p>
-        <p><a href="coach_sign_in.php" title="Coach Sign In"> Coach Sign In</p>
+        <p><a href="coach_sign_in.php" title="Coach Sign In"> Coach Sign In</a></p>
     </body>
 </html>
