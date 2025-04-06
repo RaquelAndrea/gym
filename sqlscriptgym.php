84b6757ff70aab1679a69db709f03d7089f2dbbc
RaquelAndrea <praquel2003@gmail.com>
2025-04-06 14:37:11 -0400

    Update sqlscriptgym.php

--- sqlscriptgym.php
+++ sqlscriptgym.php
@@ -1,32 +1,25 @@
-DROP TABLE gym_user;
+DROP TABLE crowd_meter;
+DROP TABLE feedback;
+DROP TABLE schedule;
+DROP TABLE FAQ;
 DROP TABLE administration;
-DROP TABLE  FAQ;
-DROP TABLE  feedback;
-DROP TABLE  schedule;
-DROP TABLE  crowd_meter;
+DROP TABLE gym_user;
 
 CREATE TABLE gym_user (
-    user_id INT(8) NOT NULL,
+    user_id INT PRIMARY KEY,
     fname VARCHAR(15) NOT NULL,
     lname VARCHAR(15) NOT NULL,
     email VARCHAR(50) NOT NULL,
     address VARCHAR(400) NOT NULL,
     phone_num VARCHAR(12) NOT NULL,
-    DOB DATE NOT NULL,
-    PRIMARY KEY (user_id)
+    DOB DATE NOT NULL
 ) ENGINE=InnoDB;
 
-CREATE TABLE administration (
-    username VARCHAR(30) PRIMARY KEY,
-    fname VARCHAR(15) NOT NULL,
-    lname VARCHAR(15) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL
-) ENGINE=InnoDB;
-
-CREATE TABLE FAQ (
-    question VARCHAR(255) PRIMARY KEY,
-    answer VARCHAR(255) NOT NULL
+CREATE TABLE crowd_meter (
+    current INT PRIMARY KEY,
+    user_id INT NOT NULL,
+    time_entery INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES gym_user(user_id)
 ) ENGINE=InnoDB;
 
 CREATE TABLE feedback (
@@ -41,34 +34,33 @@ CREATE TABLE schedule (
     announcement_text TEXT NOT NULL
 ) ENGINE=InnoDB;
 
-
-CREATE TABLE crowd_meter (
-    current INT(3) PRIMARY KEY,
-    user_id INT(8) NOT NULL,
-    time_entery VARCHAR(4),
-    FOREIGN KEY (user_id) REFERENCES gym_user(user_id)
+CREATE TABLE FAQ (
+    question VARCHAR(255) PRIMARY KEY,
+    answer VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB;
 
+CREATE TABLE administration (
+    username VARCHAR(30) PRIMARY KEY,
+    fname VARCHAR(15) NOT NULL,
+    lname VARCHAR(15) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL
+) ENGINE=InnoDB;
 
 INSERT INTO gym_user (user_id, fname, lname, email, address, phone_num, DOB) VALUES
-('87959403', 'Travis', 'James', 'tjames@gmail.com', '203 Kimberly Rd, Winchester VA', '540-533-6738', '2003-03-01');
-
-
+(87959403, 'Travis', 'James', 'tjames@gmail.com', '203 Kimberly Rd, Winchester VA', '540-533-6738', '2003-03-01');
 
 INSERT INTO crowd_meter (current, user_id, time_entery) VALUES
-('1', '87959403', '1300');
+(1, 87959403, 1300);
 
 INSERT INTO administration (username, fname, lname, email, password) VALUES
 ('coach.spring', 'Coach', 'Spring', 'coach.spring@wau.edu', 'securepass456');
 
-
 INSERT INTO FAQ (question, answer) VALUES
 ('Is the Gym only for student athletes?', 'No the gym is for everyone');
 
-
 INSERT INTO schedule (schedule_id, duration, announcement_text) VALUES
-('1', '1', '*Special Announcement: Due to the unforeseen snowstorm the gym along with the school will be closed.');
+(1, 1, '*Special Announcement: Due to the unforeseen snowstorm the gym along with the school will be closed.');
 
 INSERT INTO feedback (feedback_id, feedback_text, feedback_date) VALUES
-('1', 'Great practice today!', '2025-03-20');
-
+(1, 'Great practice today!', '2025-03-20');
