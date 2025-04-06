435c3454981b3bf86b365f07063eac680d6e107d
RaquelAndrea <praquel2003@gmail.com>
2025-04-06 14:59:21 -0400

    Update mainpagewau.php

--- mainpagewau.php
+++ mainpagewau.php
@@ -1,21 +1,11 @@
 <!DOCTYPE html>
 <html>
-<head></head>
     <body>
-        <table >
-        <tr valign = "center">
-            <td >
-            <ul>
-                <li><a href="student_sign_in.php" title="Student Sign In">Student Sign In</a>
-                <li><a href="feedback.php" title="Feedback">Feedback</a>
-                <li><a href="crowd_meter.php" title="Crowd Meter">Crowd Meter</a>
-                <li><a href="schedule.php" title="Schedule">Schedule</a>
-                <li><a href="payroll.php" title="FAQ">FAQ</a>
-                <li><a href="coach_sign_in.php" title="Coach Sign In"> Coach Sign In</a>
-               
-            </ul>
-            </td>
-        </tr>
-        </table>
+        <p><a href="student_sign_in.php" title="Student Sign In">Student Sign In</p>
+        <p><a href="feedback.php" title="Feedback">Feedback</p>
+        <p><a href="crowd_meter.php" title="Crowd Meter">Crowd Meter</p>
+        <p><a href="schedule.php" title="Schedule">Schedule</p>
+        <p><a href="payroll.php" title="FAQ">FAQ</p>
+        <p><a href="coach_sign_in.php" title="Coach Sign In"> Coach Sign In</p>
     </body>
 </html>
