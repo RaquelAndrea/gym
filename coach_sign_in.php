4aadeb0fdf9cbeb7cb5ebe16297ae949183f2954
RaquelAndrea <praquel2003@gmail.com>
2025-04-17 13:12:41 -0400

    Add files via upload (#10)
    added css and spacing changed submit to login, added md5

--- coach_sign_in.php
+++ coach_sign_in.php
@@ -1,3 +1,4 @@
+
 <?php
     // if they click go back send back to main page
     if (isset($_POST['go_back'])):
@@ -5,58 +6,79 @@
     endif;
 
     //if they click submit then connects to wconnection.php 
-    if  (isset($_POST['submit'])):
+    if  (isset($_POST['Login'])):
         require_once('wconnection.php');
         //sets variables 
-        $user_name=$_POST['username'];
-        $password=$_POST['password'];
-        
+        $user_name = $_POST['username'];
+        $password = md5($_POST['password']);
+
         //selects a query where username and password both exits and then executes it and stores it in result variable
-        $my_query="SELECT * FROM administration WHERE  username = '$user_name' AND password = '$password'";
-        $result= mysqli_query($conn, $my_query);
+        $my_query = "SELECT * FROM administration WHERE username = '$user_name' AND password = '$password'";
+        $result = mysqli_query($conn, $my_query);
 
         //either print appropriate error message or sends to appropriate location
         if (!$result)
         {
             die("Query failed: " . mysqli_error($conn));
         }
-
-        if (mysqli_num_rows($result) == 1 ) 
+        if (mysqli_num_rows($result) == 1) 
         {       
             header("Location: coach_main_page.php");
             exit();
-        } 
-
-        else
+        }    
+        else 
         { 
-            echo "<p>Invalid username or password.</p>";
+            $error = "Invalid username or password.";   
         }
 
         mysqli_close($conn);  
     endif;
 ?>
-
 <html>
     <head>
         <style>
+            input[type="text"],input[type="password"] 
+            {
+                width: 100%;
+                padding: 3%;
+                font-size: 100%;
+                box-sizing: border-box;
+            }
 
+            input.full-width
+            {
+            width: 100%;
+            padding: 3%;
+            font-size: 100%;
+            box-sizing: border-box;
+            }
         </style>
+           <link rel="stylesheet" href="master.css">
     </head>
     <body>
-        <!--Creates a form that allows for user input username and password -->
-        <h1 style="text-align: center;"> Coach Sign In</h1>
-
-        <form action="" method="post">
-            <p>Username: <input type="text" name="username" id="username" size="25" /></p>
-            <p>Password:<input type="password"name="password"  id="password"size="25" /></p>
-            <p><input type="submit" name="submit" value="Sign In"/></p>
-        </form>
+        <div class="banner">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT243RYpMJZ7i_O8XB1NJMTlB2ZybjjLCrTVg&s" alt="WAU logo" />
+            <h1>Sign In</h1>
+        </div>
+    
+        <div class="container">
+            <form action="" method="post">
+                <p>Username: <br/> <input type="text" name="username" id="username" size="25" /></p>
+                <p>Password: <br/><input type="password"name="password"  id="password"size="25" /></p>
+                <p><input type="submit" name="Login" value="Login" class="full-width"/></p>
+            </form>
+            <?php if (isset($error)) { echo "<p style='color:red;'>$error</p>"; } ?>
+        </div>
+               
         <!--Go back button-->
-        <form method="POST" action="">
-            <p>  <input type="submit" name="go_back" value="Go Back"></p>
-         </form>
+        <div style="text-align: right;">
+            <form method="POST" action="">
+                <!--<p>  <input type="submit" name="go_back" value="Go Back"></p>-->
+                <input type="submit" name="go_back" value="Go Back" class="go_back">
+            </form>
+        </div>
+        
     </body>
 </html>
-
 
 
