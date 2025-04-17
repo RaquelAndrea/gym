6088a0a7c1ec7a987ed23293d077909a47e8ffbd
RaquelAndrea <praquel2003@gmail.com>
2025-04-17 13:48:36 -0400

    Add files via upload (#11)

--- coach_main_page.php
+++ coach_main_page.php
@@ -1,39 +1,29 @@
 <?php
-if (isset($_POST['go_back'])):
-  header("Location: mainpagewau.php");
-endif;
-
+  if (isset($_POST['go_back'])):
+    header("Location: mainpagewau.php");
+  endif;
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-  <style>
-    html {
-      color-scheme: dark;
-    }
-  </style>
-</head>
-<body>
-
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT243RYpMJZ7i_O8XB1NJMTlB2ZybjjLCrTVg&s" alt="WAU logo" width="100"/>
-  <h1>Administration Page</h1>
-  <hr/>
-
-  <table>
-    <tr valign="center">
-      <td>
-        <ul>
-          <li><a href="main_sign_up.php" title="Signup">Sign Up</a></li>
-          <li><a href="faq_page.php" title="Faq">FAQ Page</a></li>
-          <li><a href="feedback.php" title="Feedback">Feedback</a></li>
-          <li><a href="change_announcement.php" title="Suggestions">Schedule Announcement</a></li>
-          <li>
-            <form method="POST" action="">
-            <input type="submit" name="go_back" value="Logout">
-            </form></li>
-        </ul>
-      </td>
-    </tr>
-  </table>
-</body>
+  <head>
+    <link rel="stylesheet" href="master.css">
+  </head>
+  <body>
+    <div class="banner">    
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT243RYpMJZ7i_O8XB1NJMTlB2ZybjjLCrTVg&s" alt="WAU logo" />
+          <h1>Administration Page</h1>
+      </div>
+    
+    <div class="container">
+      <a href="main_sign_up.php" title="Signup" class="btn-link">Sign Up</a><br/>
+      <a href="faq_page.php" title="Faq" class="btn-link">FAQ Page</a><br/>
+      <a href="feedback.php" title="Feedback" class="btn-link" >Feedback</a><br/>
+      <a href="change_announcement.php" title="Suggestions" class="btn-link" >Schedule Announcement</a>
+      
+      <form method="POST" action="">
+        <input type="submit" name="go_back" value="Logout" >
+      </form>
+    </div>
+    
+  </body>
 </html>
